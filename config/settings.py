cefebabc03e0da3debc4fba61d86061c74d7a194
Arjun4564 <arjun4564@gmail.com>
2026-06-29 19:46:24 +0530

    increase jwt token lifespan to 30 days

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,12 @@ REST_FRAMEWORK = {
 # CORS
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# JWT Configuration
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
